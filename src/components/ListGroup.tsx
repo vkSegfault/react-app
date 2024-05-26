21a4c41b88db8b541098139795d7f4535d6b1a5d
vkSegfault <45318345+vkSegfault@users.noreply.github.com>
2024-05-26 11:40:19 +0200

    upd

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -7,7 +7,7 @@ function ListGroup() {
     <Fragment>
       <h1>Welcome</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
+        <li className="list-group-item active">An item</li>
         <li className="list-group-item">A second item</li>
         <li className="list-group-item">A third item</li>
         <li className="list-group-item">A fourth item</li>
